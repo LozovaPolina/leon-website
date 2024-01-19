d343d211c6fa98cbe3bca1547710ff481bf4c6e3
Polina Lozova <1polinalozovaya1@gmail.com>
2024-01-19 08:27:30 +0300

    add changes playVideo

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,8 @@ import Forms from "./modules/forms";
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    const player = new VideoPlayer('.showup .play', '.overlay')
-    player.init();
-
+    new VideoPlayer('.showup .play', '.overlay').init();
+    new VideoPlayer('.module__video-item .play', '.overlay').init();
 
     const slider = new MainSlider({
         container: '.page',
